4177c8524103be3693a5751017bf986ac115331f
Son Nguyen <dinh.son.nguyen@gmail.com>
2019-04-27 22:56:12 +0700

    Initial commit

---
+++ GivenWhenThenTemplateToCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GivenWhenThenTemplateToCSharp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
